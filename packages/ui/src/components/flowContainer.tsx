1ca1894a6a6a1041430e8edaae5310cec6487345
Leandro Gonçalves <leandrogoncalvespr@gmail.com>
2026-03-13 15:13:12 -0300

    feat: add global styles export to package.json and remove direct import in FlowContainer

--- packages/ui/src/components/flowContainer.tsx
+++ packages/ui/src/components/flowContainer.tsx
@@ -18,7 +18,6 @@ import { useCallback, useRef } from "react";
 import { FlowNodeContextProvider } from "../context/FlowNodeContext";
 import type { useFlowNodes } from "../hooks/useFlowNodes";
 import { useAddDefaultNode } from "../nodes/DefaultNode/useAddDefaultNode";
-import "../styles/globals.css";
 import { AddNodeSheet } from "./add-node-sheet";
 import { EditNodeDialog } from "./edit-node-dialog";
 import { Toaster } from "./sonner";
